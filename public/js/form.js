95fbb709058f76eecb05adf99f8a343ab3618a05
hosamelshazly92 <hosamelshazly92@gmail.com>
2022-03-03 20:51:18 +0200

    edit form script - reset buttom

--- public/js/form.js
+++ public/js/form.js
@@ -1,13 +1,7 @@
 const resetButton = document.getElementById("reset");
-const typeInput = document.getElementById("type");
-const questionInput = document.getElementById("question");
-const firstInput = document.getElementById("first");
-const secondInput = document.getElementById("second");
-const thirdInput = document.getElementById("third");
 
 resetButton.addEventListener("click", function () {
   const inputs = document.getElementsByTagName("input");
   const arrInputs = Array.from(inputs);
-  //   console.log(arrInputs);
   arrInputs.forEach((elm) => (elm.value = ""));
 });
